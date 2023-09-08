e49ebac9eb4a28c3196ae5a3ab129481b9abff3a
Sunroses <deli73@users.noreply.github.com>
2023-09-07 17:41:56 -0700

    made linear 1.19.4 version

--- src/main/java/xyz/sunrose/xps/XpShare.java
+++ src/main/java/xyz/sunrose/xps/XpShare.java
@@ -6,16 +6,16 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.ArgumentCommandNode;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.EntitySelector;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.text.LiteralTextContent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,124 +23,51 @@ public class XpShare implements ModInitializer {
     public static final String MODID = "xpshare";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-    public static final String LV_GIVEN = "my-levels";
-    public static final String LV_RECIEVED = "levels";
+    public static final int XP_PER_LEVEL = 46;
 
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             LiteralCommandNode<ServerCommandSource> xpshareNode = CommandManager
                     .literal("xpshare")
-                    .build();
-
-            LiteralCommandNode<ServerCommandSource> levelsGivenNode = CommandManager
-                    .literal(LV_GIVEN)
-                    .build();
-
-            LiteralCommandNode<ServerCommandSource> levelsRecievedNode = CommandManager
-                    .literal(LV_RECIEVED)
-                    .build();
-
-            LiteralCommandNode<ServerCommandSource> pointsNode = CommandManager
-                    .literal("points")
                     .build();
 
             ArgumentCommandNode<ServerCommandSource, EntitySelector> levelsGivenPlayerNode = CommandManager
                     .argument("player", EntityArgumentType.player())
                     .build();
-            ArgumentCommandNode<ServerCommandSource, EntitySelector> levelsRecievedPlayerNode = CommandManager
-                    .argument("player", EntityArgumentType.player())
-                    .build();
-            ArgumentCommandNode<ServerCommandSource, EntitySelector> pointsPlayerNode = CommandManager
-                    .argument("player", EntityArgumentType.player())
-                    .build();
 
             ArgumentCommandNode<ServerCommandSource, Integer> numLevelsGivenNode = CommandManager
                     .argument("levels", IntegerArgumentType.integer(0))
-                    .executes((context) -> give(context, TransferType.LEVELS_GIVE))
-                    .build();
-            ArgumentCommandNode<ServerCommandSource, Integer> numLevelsRecievedNode = CommandManager
-                    .argument("levels", IntegerArgumentType.integer(0))
-                    .executes((context -> give(context, TransferType.LEVELS_RECIEVE)))
-                    .build();
-            ArgumentCommandNode<ServerCommandSource, Integer> numPointsNode = CommandManager
-                    .argument("points", IntegerArgumentType.integer(0))
-                    .executes(context -> give(context, TransferType.POINTS))
+                    .executes((context) -> give(context))
                     .build();
 
 
             dispatcher.getRoot().addChild(xpshareNode);
 
-            xpshareNode.addChild(levelsGivenNode);
-            levelsGivenNode.addChild(levelsGivenPlayerNode);
+            xpshareNode.addChild(levelsGivenPlayerNode);
             levelsGivenPlayerNode.addChild(numLevelsGivenNode);
-
-            xpshareNode.addChild(levelsRecievedNode);
-            levelsRecievedNode.addChild(levelsRecievedPlayerNode);
-            levelsRecievedPlayerNode.addChild(numLevelsRecievedNode);
-
-            xpshareNode.addChild(pointsNode);
-            pointsNode.addChild(pointsPlayerNode);
-            pointsPlayerNode.addChild(numPointsNode);
         });
     }
 
-    private enum TransferType {
-        LEVELS_GIVE, LEVELS_RECIEVE, POINTS
-    }
-
-    public static int give(CommandContext<ServerCommandSource> context, TransferType type) throws CommandSyntaxException{
+    public static int give(CommandContext<ServerCommandSource> context) throws CommandSyntaxException{
         ServerCommandSource src = context.getSource();
         Entity e = src.getEntity();
         if(e instanceof ServerPlayerEntity giver) {
             ServerPlayerEntity reciever = context.getArgument("player", EntitySelector.class).getPlayer(src);
-            int levelsGive;
-            int pointsGive;
-            int leftoverPoints = (int) (giver.experienceProgress * giver.getNextLevelExperience());
-            int pointsHave = levelRangeAsPoints(0, giver.experienceLevel) + leftoverPoints; //totalExperience isn't updated fsr, fml
-            Text feedback;
-            Text message;
-            switch (type){
-                case LEVELS_GIVE -> { //give the xp required to remove some number of levels *from the giver*
-                    levelsGive = context.getArgument("levels", Integer.class);
-                    int levelsHave = giver.experienceLevel;
-                    pointsGive = levelRangeAsPoints(levelsHave-levelsGive, levelsHave);
-                    feedback = new LiteralText("Given "+levelsGive+" of your levels to "+reciever.getDisplayName()+" ("+pointsGive+" points)");
-                    message = new LiteralText("Recieved xp from "+giver.getDisplayName()); //todo: tell the level amount recieved??
-                }
-                case LEVELS_RECIEVE -> { //give the xp requried to add some number of levels *to the reciever*
-                    levelsGive = context.getArgument("levels", Integer.class);
-                    int targetLevelsHave = reciever.experienceLevel;
-                    pointsGive = levelRangeAsPoints(targetLevelsHave, targetLevelsHave+levelsGive);
-                    feedback = new LiteralText("Shared xp to "+reciever.getDisplayName()+" to increase their levels by "+levelsGive+" ("+pointsGive+" points)");
-                    message = new LiteralText("Recieved "+levelsGive+" levels from "+giver.getDisplayName());
-                }
-                default -> { //just put in point values directly
-                    pointsGive = context.getArgument("points", Integer.class);
-                    feedback = new LiteralText("Given "+pointsGive+" experience points to "+reciever.getDisplayName());
-                    message = new LiteralText("Recieved "+pointsGive+" experience points from "+giver.getDisplayName());
-                }
-            }
+            int levelsToGive = context.getArgument("levels", Integer.class);
+            int levelsHave = giver.experienceLevel;
 
-            if(pointsHave >= pointsGive) {
-                giver.addExperience(-pointsGive);
-                reciever.addExperience(pointsGive);
+
+            if (levelsHave >= levelsToGive) {
+                Text feedback = MutableText.of(new LiteralTextContent("Given " + levelsToGive + " levels to " + reciever.getDisplayName().getString()));
+                Text message = MutableText.of(new LiteralTextContent("Recieved " + levelsToGive + " levels from " + giver.getDisplayName().getString()));
+                giver.addExperience(-levelsToGive * XP_PER_LEVEL);
+                reciever.addExperience(levelsToGive * XP_PER_LEVEL);
                 src.sendFeedback(feedback, true);
-                reciever.sendSystemMessage(message, giver.getUuid());
+                reciever.sendMessage(message);
                 return 1;
             }
         }
         return 0;
-    }
-
-    protected static int levelRangeAsPoints(int from, int to){
-        int total = 0;
-        if(to < from){return 0;}
-        for (int i = from; i < to; i++) { //mojang whyyyy
-            if(i<=15) {total += 2 * i + 7;}
-            else if(i<=30) {total += 5 * i - 38;}
-            else {total += 9 * i - 158;}
-        }
-        return total;
     }
 }
